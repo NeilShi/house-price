b5bbde6a1548fa84c4cc46b1d9b6d76efa847ee6
neilshi <627911861@qq.com>
2019-08-15 17:33:53 +0800

    add month and year

--- src/main/java/houseprice/nonentity/vo/CommunityInfoVo.java
+++ src/main/java/houseprice/nonentity/vo/CommunityInfoVo.java
@@ -1,12 +1,30 @@
 package houseprice.nonentity.vo;
 
 public class CommunityInfoVo {
+    private int year;
+    private int month;
     private String city;
     private String district;
     private String area;
     private String name;
     private String price;
     private String on_sale;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
 
     public String getCity() {
         return city;
