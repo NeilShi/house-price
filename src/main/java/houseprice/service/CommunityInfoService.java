fd8212f6573372a16b692fcf9a1abcabb566efbb
neilshi <627911861@qq.com>
2019-08-22 17:30:56 +0800

    add communityDao

--- src/main/java/houseprice/service/CommunityInfoService.java
+++ src/main/java/houseprice/service/CommunityInfoService.java
@@ -1,7 +1,10 @@
 package houseprice.service;
 
+import houseprice.dao.CommunityDao;
+import houseprice.domain.Community;
 import houseprice.nonentity.criteria.SearchCommunityCriteria;
 import houseprice.nonentity.vo.CommunityInfoVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -9,20 +12,31 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 public class CommunityInfoService {
+    @Autowired
+    private CommunityDao communityDao;
+
     private static final String URL = "http://127.0.0.1:5000/community";
-    public List<CommunityInfoVo> searchCommunityInfo(SearchCommunityCriteria criteria) {
+
+    public List<CommunityInfoVo> crawlCommunityInfo(SearchCommunityCriteria criteria) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<SearchCommunityCriteria> request = new HttpEntity<>(criteria, headers);
         ResponseEntity<List<CommunityInfoVo>> response = restTemplate.exchange(URL, HttpMethod.POST, request,
                 new ParameterizedTypeReference<List<CommunityInfoVo>>() {});
-        return response.getBody();
+        List<CommunityInfoVo> resultList = response.getBody();
+        return resultList;
+    }
+
+    @Transactional(readOnly = true)
+    public Community searchCommunity(String id) {
+        return communityDao.findCommunityById(id);
     }
 }
