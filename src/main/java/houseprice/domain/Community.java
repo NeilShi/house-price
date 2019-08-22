42075b69e2637781d5eb3c02a635e7cd3e1d0035
neilshi <627911861@qq.com>
2019-08-22 17:29:43 +0800

    add Community.java

---
+++ src/main/java/houseprice/domain/Community.java
@@ -0,0 +1,85 @@
+package houseprice.domain;
+
+public class Community {
+    private int id;
+    private int year;
+    private int month;
+    private String city;
+    private String district;
+    private String area;
+    private String name;
+    private String price;
+    private String on_sale;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getOn_sale() {
+        return on_sale;
+    }
+
+    public void setOn_sale(String on_sale) {
+        this.on_sale = on_sale;
+    }
+}
