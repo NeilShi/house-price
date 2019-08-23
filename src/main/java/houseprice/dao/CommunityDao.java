f2eb534608acad9ef8a9c0fec3e4510dcb9f3842
neilshi <627911861@qq.com>
2019-08-23 15:03:40 +0800

    add insert sql

--- src/main/java/houseprice/dao/CommunityDao.java
+++ src/main/java/houseprice/dao/CommunityDao.java
@@ -1,12 +1,30 @@
 package houseprice.dao;
 
 import houseprice.domain.Community;
+import houseprice.nonentity.vo.CommunityInfoVo;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface CommunityDao {
+
     @Select("SELECT * FROM community WHERE id = #{id}")
     Community findCommunityById(@Param("id") String id);
+
+    @Select("SELECT * FROM community WHERE name = #{name}")
+    Community findCommunityByName(@Param("name") String name);
+
+    @Insert({
+            "<script>",
+            "insert ignore into community(year, month, city, district, area, name, price, on_sale) values",
+            "<foreach collection='communityInfoVoList' item='item' index='index' separator=','>",
+            "(#{item.year}, #{item.month}, #{item.city}, #{item.district}, #{item.area}, #{item.name}, #{item.price}, #{item.on_sale})",
+            "</foreach>",
+            "</script>"
+    })
+    int insertCommunityList(@Param(value = "communityInfoVoList") List<CommunityInfoVo> communityInfoVoList);
 }
