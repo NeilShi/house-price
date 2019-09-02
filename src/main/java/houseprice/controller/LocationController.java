7d10afa9fc9adc7fb12e4a53227f5f19505c63d1
neilshi <627911861@qq.com>
2019-09-02 16:31:07 +0800

    add LocationController.java

---
+++ src/main/java/houseprice/controller/LocationController.java
@@ -0,0 +1,34 @@
+package houseprice.controller;
+
+import houseprice.nonentity.criteria.SearchLocationCriteria;
+import houseprice.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/location")
+public class LocationController {
+
+    @Autowired
+    LocationService locationService;
+
+    @RequestMapping("/city")
+    public List<Map<String, String>> searchCity() {
+        return locationService.searchCity();
+    }
+
+    @RequestMapping("/district")
+    public List<Map<String, String>> searchDistrict(@RequestBody SearchLocationCriteria criteria) {
+        return locationService.searchDistrict(criteria);
+    }
+
+    @RequestMapping("/area")
+    public List<Map<String, String>> searchArea(@RequestBody SearchLocationCriteria criteria) {
+        return locationService.searchArea(criteria);
+    }
+}
