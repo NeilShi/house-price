26ef1b4e4e7adb5274c292fb20752f8134346493
neilshi <627911861@qq.com>
2019-09-02 16:31:58 +0800

    add SearchLocationCriteria.java

---
+++ src/main/java/houseprice/nonentity/criteria/SearchLocationCriteria.java
@@ -0,0 +1,23 @@
+package houseprice.nonentity.criteria;
+
+public class SearchLocationCriteria {
+
+    private String city;
+    private String district;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+}
