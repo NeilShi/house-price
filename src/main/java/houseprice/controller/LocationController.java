f236f114be555a0b0ededd7cbcc603fc42bd4919
neilshi <627911861@qq.com>
2019-09-11 17:16:43 +0800

    implement CrossOrigin

--- src/main/java/houseprice/controller/LocationController.java
+++ src/main/java/houseprice/controller/LocationController.java
@@ -3,6 +3,7 @@ package houseprice.controller;
 import houseprice.nonentity.criteria.SearchLocationCriteria;
 import houseprice.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/location")
 public class LocationController {
